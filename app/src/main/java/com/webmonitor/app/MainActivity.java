bab61ca02cae69c0ff2d4ad9ace7aaff0ac7d962
Leonardo Magno Ribeiro Carvalho <leuribeiru@gmail.com>
2021-03-22 14:22:24 -0300

    upload notifications basic

--- app/src/main/java/com/webmonitor/app/MainActivity.java
+++ app/src/main/java/com/webmonitor/app/MainActivity.java
@@ -3,6 +3,8 @@ package com.webmonitor.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(adapter);
 
 
+        /************** cria um NotificationChanel *********/
+
+        this.createNotificationChannel();
+
         /************* cria um AlarmManager ***************/
 
         alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
@@ -41,8 +47,34 @@ public class MainActivity extends AppCompatActivity {
         Intent alarmIntent = new Intent(this, Notifica.class);
         pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
 
-        //agenda o alarme de 10 em 10 seg
-        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, 0, 10000, pendingIntent);
+        //agenda o alarme de 60 em 60 seg
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Log.i("Alarme", "setInexactRepeating");
+            alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, 0, 10000, pendingIntent);
+            //Log.i("Alarme", "setAndAllowWhileIdle");
+            //alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Log.i("Alarme", "setExact");
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        } else {
+            Log.i("Alarme", "set");
+            alarmManager.set(AlarmManager.RTC_WAKEUP, 0, pendingIntent);
+        }
+    }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "webmonitor";
+            String description = "descrição do canal do webmonitor";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("123", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/webmonitor/app/Notifica.java
+++ app/src/main/java/com/webmonitor/app/Notifica.java
@@ -1,10 +1,12 @@
 package com.webmonitor.app;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -17,12 +19,25 @@ public class Notifica extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+
         if (intent.getAction() != null && intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
             Intent serviceIntent = new Intent(context, NotificaService.class);
             context.startService(serviceIntent);
         } else {
             Log.i("Alarme", "Disparou");
-            Toast.makeText(context.getApplicationContext(), "Olá, eu sou o alarme!", Toast.LENGTH_LONG).show();
+
+            //Toast.makeText(context.getApplicationContext(), "Olá, eu sou o alarme!", Toast.LENGTH_LONG).show();
+            NotificationCompat.Builder notBuilder= new NotificationCompat.Builder(context, "123")
+                    .setSmallIcon(R.mipmap.ic_launcher_round)
+                    .setContentTitle("webmonitor")
+                    .setContentText("Exibindo uma notificação pelo AlarmManager")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setAutoCancel(true);
+
+            NotificationManager notManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notManager.notify(100, notBuilder.build());
         }
     }
+
+
 }
